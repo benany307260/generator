86f5d386fde5b6495eb7605df0e3a53587e1e135
benany307260 <307260621@qq.com>
2018-11-09 20:15:44 +0800

    创建项目

---
+++ generator/src/main/java/com/bentest/generator/App.java
@@ -0,0 +1,13 @@
+package com.bentest.generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
